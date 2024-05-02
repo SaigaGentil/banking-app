7df209ebfd0fa4987642c6410cfb88613fc24d14
SaigaGentil <contact@brunoaxelkamere.com>
2024-05-02 17:58:19 +0200

    Implement sidebar, doughnut chart, and current balance view

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,15 @@
+import SideBar from "@/components/SideBar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Bruno", lastName: "Kamere" };
+
   return (
-    <main>
-      SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+      <SideBar user={loggedIn} />
       {children}
     </main>
   );
